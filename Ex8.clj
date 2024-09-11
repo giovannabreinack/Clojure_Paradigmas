ec78ec055d554a5e1daa70319d6a00f9d1307d6f
Gio <105086221+giovannabreinack@users.noreply.github.com>
2024-09-11 09:57:48 -0300

    Delete Ex8.clj

--- Ex8.clj
+++
@@ -1,7 +0,0 @@
-(defn filtroImpar [palavras]
-  (->> palavras
-       (filter #(odd? (count %)))  
-       (map count)))               
-
-(def palavras ["clojure" "alface" "brigadeiro" "vida" "jogo" "jovens" "mes"])
-(println (filtroImpar palavras))
